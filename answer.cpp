59889d46dc2e73ecc384048b9d497216a3fc9663
yusong chen <3149512122@qq.com>
2023-06-01 15:00:22 +0800

    完成

--- answer.cpp
+++ answer.cpp
@@ -7,7 +7,7 @@ Answer::Answer(QString _name) : Player(_name)
 {
     playerType = PlayerType::Answer;
     LoadData();
-    max = 10 * pow(2,grade/5 +2);
+    max = 10 * pow(2, grade / 5 + 2);
 }
 
 void Answer::UpdateData()
@@ -15,22 +15,22 @@ void Answer::UpdateData()
     data[SaveManager::LEVEL_NUM] = levelNum;
     data[SaveManager::GRADE] = grade;
     data[SaveManager::EXP] = exp;
-    SaveManager::SavePlayerAnswer(name,data);
+    SaveManager::SavePlayerAnswer(name, data);
 }
 void Answer::LoadData()
 {
     SaveManager::LoadPlayerAnswer(name, data);
     exp = data.value(SaveManager::EXP).toInt();
-    grade =  data.value(SaveManager::GRADE).toInt();
+    grade = data.value(SaveManager::GRADE).toInt();
     levelNum = data.value(SaveManager::LEVEL_NUM).toInt();
 }
 
 void Answer::UptateEXP(int _level, int _length)
 {
-    max = 10 * pow(2,grade/5 +2);
-    if(_length == -1)
+    max = 10 * pow(2, grade / 5 + 2);
+    if (_length == -1)
     {
-        if(_level <= levelNum)
+        if (_level <= levelNum)
             exp = exp + 5 * _level;
         else
             exp = exp + 10 * _level;
@@ -39,7 +39,7 @@ void Answer::UptateEXP(int _level, int _length)
     {
         exp = exp + 5 * _length;
     }
-    if(exp >= max)
+    if (exp >= max)
     {
         exp = exp - max;
         UpdateGrade(1);
@@ -48,11 +48,11 @@ void Answer::UptateEXP(int _level, int _length)
 
 void Answer::UptateEXP(int _exp)
 {
-    max = 10 * pow(2,grade/5 +2);
-    if(_exp>0)
+    max = 10 * pow(2, grade / 5 + 2);
+    if (_exp > 0)
     {
-        exp+=_exp;
-        if(exp >= max)
+        exp += _exp;
+        if (exp >= max)
         {
             exp -= max;
             UpdateGrade(1);
@@ -60,14 +60,14 @@ void Answer::UptateEXP(int _exp)
     }
     else
     {
-        exp -= _exp;
-        while(exp < 0)
+        exp += _exp;
+        while (exp < 0)
         {
             UpdateGrade(-1);
-            max = 10 * pow(2,grade/5 +2);
+            max = 10 * pow(2, grade / 5 + 2);
             exp = max + exp;
-            //不够减的情况
-            if(grade == 0 && exp <0)
+            // 不够减的情况
+            if (grade == 0 && exp < 0)
             {
                 exp = 0;
                 break;
